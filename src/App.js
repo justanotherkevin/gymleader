9317960f7c52533d849f0975b256457fb22f17bf
kevin hu <hu.kevin.w@gmail.com>
2017-10-19 17:37:33 -0400

    Add react router and their route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
+import { Switch, Route, BrowserRouter,Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Home from './components/Home';
+import Gym from './components/Gym';
 
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/gym">Gym</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/gym" component={Gym}/>
+    </div>
+  </BrowserRouter>
+)
 export default App;
