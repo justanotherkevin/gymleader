302602d6542d4cab91fb933d2dcf1ce12f3e01c6
kevin hu <hu.kevin.w@gmail.com>
2017-10-19 18:11:35 -0400

    Add home map component

---
+++ src/components/map/homeMap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const homeMap = React.createClass({
+    render() {
+        return (
+            <div className="home_map">
+                <p>this is where the map will be </p> 
+            </div>
+        )
+    }
+})
