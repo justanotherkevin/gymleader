dd6ad576070951267c88c85fb44421870b10daff
kevin hu <hu.kevin.w@gmail.com>
2017-10-19 17:36:11 -0400

    Add component GYM and HOME

---
+++ src/components/Gym.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Gym extends Component {
+  render() {
+    return (
+      <div>
+          <h1>This is the Gym</h1>
+      </div>
+    );
+  }
+}
+export default Gym;
