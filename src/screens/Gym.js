e4952dceb6195fb1a64e67707d6bdb7616143850
kevin hu <hu.kevin.w@gmail.com>
2017-10-19 18:57:55 -0400

    Add function to adf gym to state

--- src/screens/Gym.js
+++ src/screens/Gym.js
@@ -1,12 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {FIREconfig} from '../config/fireConfig';
+import firebase from 'firebase/app';
 
 class Gym extends Component {
-  render() {
-    return (
-      <div>
-          <h1>This is the Gym</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            gyms: [],
+        };
+
+        this.app = firebase.initializeApp(FIREconfig);
+        this.db = this.app.database().ref.child('gyms');
+        this.addGym = this.addGym.bind(this)
+    };
+
+    componentWillMount() {
+        const previousGyms = this.state.gyms
+    }
+
+    addGym(gym) {
+        // gym need to be an object with ID and stuff. ex. {id:1, name: 'kickass'}
+        const currentGyms = this.state.gyms;
+        currentGyms.push(gym);
+        this.setState({
+            gyms: currentGyms
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>This is the Gym</h1>
+            </div>
+        );
+    }
 }
 export default Gym;
