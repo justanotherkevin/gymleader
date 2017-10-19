c50fb454f0d236c359075d06c05b24009780d3e7
kevin hu <hu.kevin.w@gmail.com>
2017-10-19 19:03:01 -0400

    Import gymmap component to home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Gymmap from '../components/map/homeMap';
 
 class Home extends Component {
   render() {
     return (
       <div>
           <h1>This is the Home</h1>
+          <Gymmap/>
       </div>
     );
   }
