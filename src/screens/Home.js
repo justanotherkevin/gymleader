837adee9b11eefd1738a6539c31b7e1215b7bed6
kevin hu <hu.kevin.w@gmail.com>
2017-10-20 14:22:22 -0400

    Test for rendering constant

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,10 +3,14 @@ import Gymmap from '../components/map/homeMap';
 
 class Home extends Component {
   render() {
+    const hello = () => {
+      return <h2>this is some random constant</h2>
+    }
     return (
       <div>
           <h1>This is the Home</h1>
           <Gymmap/>
+          {hello()}
       </div>
     );
   }
