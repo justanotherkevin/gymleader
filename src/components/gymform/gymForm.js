dffed91b3140ac05eab6fa6f1ed4c4b4f2e868b1
kevin hu <hu.kevin.w@gmail.com>
2017-10-20 19:57:24 -0400

    Adding a gymfrom to add grm

---
+++ src/components/gymform/gymForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class GymForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newGymInfo: '',
+        };
+        this.handleUserInput = this.handleUserInput.bind(this);
+        this.handleUserSubmit = this.handleUserSubmit.bind(this);
+    };
+    handleUserInput(e) {
+        this.setState({
+            newGymInfo: e.target.value,
+        })
+    };
+    handleUserSubmit() {
+        this.props.addGym(this.state.newGymInfo)
+        this.setState({
+            newGymInfo: '',
+        })
+    }
+    render() {
+        return(
+            <div className="gymForm_wrapper">
+                <input className="gymForm"
+                    placeholder="add a gym..."
+                    value={this.state.newGymInfo}
+                    onChange={this.handleUserInput}
+                />
+            <button className="gymForm_button"
+                onClick={this.handleUserSubmit}>gym button</button>
+            </div>
+        )
+    };
+};
+export default GymForm;
