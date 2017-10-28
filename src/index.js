c65a2dd545084db909f7c8e4a471efca521fbd16
kevin hu <hu.kevin.w@gmail.com>
2017-10-28 18:14:18 -0400

    Import normalize css stylesheets from react-addons-css-transition-group package

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 // import stylesheets
 import '@blueprintjs/core/dist/blueprint.css';
+import "normalize.css/normalize.css"
 
 ReactDOM.render(<App/>,document.getElementById('root'));
 registerServiceWorker();
