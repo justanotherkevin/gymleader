4a7e44c838d26e439e78fc9f6a6456ec373027bf
kevin hu <hu.kevin.w@gmail.com>
2017-10-28 18:15:00 -0400

    Change constant to Class for authentication w/facebook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, BrowserRouter, Link } from 'react-router-dom';
+import { fire } from './config/fireConfig';
+
 import Header from './components/header';
 import Login from './components/login';
-import './stylesheets/app.css';
-
 import Home from './pages/Home';
 import Gym from './pages/Gym';
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <Header/>
-      <hr/>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/login" component={Login}/>
-      <Route path="/gym" component={Gym}/>
-    </div>
-  </BrowserRouter>
-)
+import './stylesheets/app.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        // this.setCurrentUser = this.setCurrentUser.bind(this);
+        this.state = {
+            authenticated: false,
+            currentUser: null,
+        }
+    }
+    componentWillMount() {
+        this.removeAuthListener = fire.auth().onAuthStateChanged((user) => {
+            if (user) {
+                console.log(user.displayName)
+                this.setState({
+                    authenticated: true,
+                    currentUser: user,
+                })
+            } else {
+                this.setState({
+                    authenticated: false,
+                })
+            }
+        })
+    }
+
+    componentWillUnmount() {
+        this.removeAuthListener();
+    }
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Header authenticated={this.state.authenticated}
+                            user={this.state.currentUser}/>
+                        <hr/>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/login" component={Login} />
+                        <Route path="/gym" component={Gym} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        )
+    }
+}
+
 export default App;
