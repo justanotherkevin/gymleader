d0ce5c0afe13bfaaf9d06695fd07b27630cb62ef
kevin hu <hu.kevin.w@gmail.com>
2017-10-28 14:43:01 -0400

    Import stylesheets from blueprint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// import stylesheets
+import '@blueprintjs/core/dist/blueprint.css';
 
 ReactDOM.render(<App/>,document.getElementById('root'));
 registerServiceWorker();
