8d029b5463c29d3a53ad8b23b94535c89b8ef01a
kevin hu <hu.kevin.w@gmail.com>
2017-10-30 18:31:35 -0400

    Fix spacing with prettier

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import Gymmap from '../components/homeMap';
+import React, { Component } from "react";
+import Gymmap from "../components/homeMap";
 
 class Home extends Component {
-  render() {
-    const hello = () => {
-      return <h2>this is some random constant</h2>
+    render() {
+        const hello = () => {
+            return <h2>this is some random constant</h2>;
+        };
+        return (
+            <div>
+                <h1>This is the Home</h1>
+                <Gymmap />
+                {hello()}
+            </div>
+        );
     }
-    return (
-      <div>
-          <h1>This is the Home</h1>
-          <Gymmap/>
-          {hello()}
-      </div>
-    );
-  }
 }
 export default Home;
