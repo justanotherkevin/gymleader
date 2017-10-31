9b4d9c86f275d2da2a2e5de72ceac02254677f77
kevin hu <hu.kevin.w@gmail.com>
2017-10-31 15:40:12 -0400

    Create gyminfo.js file

---
+++ src/components/gymInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class GymInfo extends React.component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <p>this is new gyminfo</p>
+            </div>
+        )
+    }
+}
+export default GymInfo
