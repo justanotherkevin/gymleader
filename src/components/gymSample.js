db713e29344bb336eef7bed60b2407ea82e8e543
kevin hu <hu.kevin.w@gmail.com>
2017-10-31 16:23:30 -0400

    Pass function to set selectedgym to gymsample

--- src/components/gymSample.js
+++ src/components/gymSample.js
@@ -4,19 +4,17 @@ import { Button, Position, Toaster, Intent } from "@blueprintjs/core";
 class GymSample extends React.Component {
     constructor(props){
         super(props);
-        this.gymName = props.gymName;
-        this.gymId = props.gymId;
-        this.state = {
-            selectedGym: null,
-        }
+        this.sampleGym = props.sampleGym
     }
-    render(props){
+    render(){
         return (
             <div className="single-gym-wrapper">
-                <p> gym name: { this.gymName }</p>
-                <Button type="Button" className="pt-button">
+                <p> gym name: { this.sampleGym.name }</p>
+                <Button type="Button" className="pt-button" onClick={() => {
+                    this.props.selecteGym(this.sampleGym)
+                } }>
                     click me
-                    <span class="pt-icon-standard pt-icon-arrow-right pt-align-right"></span>
+                    <span className="pt-icon-standard pt-icon-arrow-right pt-align-right"></span>
                 </Button>
             </div>
         )
