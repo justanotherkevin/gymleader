9f52bcef014032052513523599fc021197959295
kevin hu <hu.kevin.w@gmail.com>
2017-11-01 16:38:14 -0400

    Change loading spinner to loading bar

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,22 @@ import { Link } from 'react-router-dom';
 
 class Header extends Component {
     render () {
+        const isLoading = () => {
+            if (this.props.loading) {
+                return (
+                    <div className="pt-progress-bar pt-intent-primary .modifier">
+                        <div className="pt-progress-meter"></div>
+                    </div>
+                )
+            }
+        }
         return (
             <div className="header_wrapper">
-                <ul className="right_side">
+                <ul className="header_right">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/gym">Gym</Link></li>
                 </ul>
-                <ul className="left_side">
+                <ul className="header_left">
                     { this.props.authenticated
                         ? (
                             <li>{this.props.user.displayName} <Link className="pt-button pt-minimal pt-icon-log-out" to="/logout">Logout</Link></li>
@@ -20,6 +29,7 @@ class Header extends Component {
                         )
                     }
                 </ul>
+                { isLoading() }
             </div>
         )
     }
