dec0931721d1461c953b1fb1233b73dba7aa8ba0
kevin hu <hu.kevin.w@gmail.com>
2017-11-03 16:22:02 -0400

    Add gym location for view

--- src/components/gymSample.js
+++ src/components/gymSample.js
@@ -4,15 +4,21 @@ import { Button, Position, Toaster, Intent } from "@blueprintjs/core";
 class GymSample extends React.Component {
     constructor(props){
         super(props);
-        this.sampleGym = props.sampleGym
     }
     render(){
         return (
             <div className="single-gym-wrapper">
-                <p> gym name: { this.sampleGym.name }</p>
-                <Button type="Button" className="pt-button" onClick={() => {
-                    this.props.selecteGym(this.sampleGym)
-                } }>
+                <p>
+                    gym name: {this.props.sampleGym.name }
+                </p>
+                <p>
+                    gym location: {this.props.sampleGym.location }
+                </p>
+                <Button type="Button"
+                    className="pt-button"
+                    onClick={() => {
+                        this.props.selecteGym(this.props.sampleGym)
+                    } }>
                     click me
                     <span className="pt-icon-standard pt-icon-arrow-right pt-align-right"></span>
                 </Button>
