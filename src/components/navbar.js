14167d33c62efc086a80211e61205ad9c7ec9dd5
kevin hu <hu.kevin.w@gmail.com>
2017-11-27 12:09:33 -0500

    Spacign with pritter

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,47 +1,78 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
-    render () {
-        const isLoading = () => {
-            if (this.props.loading) {
-                return (
-                    <div className="pt-progress-bar pt-intent-primary .modifier">
-                        <div className="pt-progress-meter"></div>
-                    </div>
-                )
-            }
-        }
+  render() {
+    const isLoading = () => {
+      if (this.props.loading) {
         return (
-            <nav className="navbar navbar-dark navbar-expand-lg fixed-top" id="mainNav">
-                <div className="container-fluid container"><a className="navbar-brand" href="#">GymLeaders</a>
-                    <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarResponsive">
-                        <ul className="nav navbar-nav text-uppercase ml-auto">
-                            <li className="nav-item" role="presentation">
-                                <a className="nav-link active nav-link js-scroll-trigger" href="/">Home </a>
-                            </li>
-                            <li className="nav-item" role="presentation">
-                                <a className="nav-link nav-link js-scroll-trigger" href="/gym">Gym </a>
-                            </li>
-                            { this.props.authenticated
-                                ? (
-                                    <li>{this.props.user.displayName} <Link className="nav-link nav-link pt-icon-log-out" to="/logout">Logout</Link></li>
-                                )
-                                : (
-                                    <li><Link className="nav-link nav-link  pt-icon-log-in" to="/login">Login</Link></li>
-                                )
-                            }
-                        </ul>
-                    </div>
-                </div>
-                { isLoading() }
-            </nav>
-        )
-    }
+          <div className="pt-progress-bar pt-intent-primary .modifier">
+            <div className="pt-progress-meter" />
+          </div>
+        );
+      }
+    };
+
+    // document.getElementById("nav-link-gym").href
+    return (
+      <nav
+        className="navbar navbar-dark navbar-expand-lg fixed-top"
+        id="mainNav"
+      >
+        <div className="container-fluid container">
+          <a className="navbar-brand" href="#">
+            GymLeaders
+          </a>
+          <button
+            className="navbar-toggler"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="nav navbar-nav text-uppercase ml-auto">
+              <li className="nav-item" role="presentation">
+                <a
+                  className="nav-link active nav-link js-scroll-trigger"
+                  href="/"
+                >
+                  Home{" "}
+                </a>
+              </li>
+              <li className="nav-item" role="presentation">
+                <a id="nav-link-gym" className="nav-link nav-link js-scroll-trigger" href="/gym">
+                  Gym{" "}
+                </a>
+              </li>
+              {this.props.authenticated ? (
+                <li>
+                  {this.props.user.displayName}{" "}
+                  <Link
+                    className="nav-link nav-link pt-icon-log-out"
+                    to="/logout"
+                  >
+                    Logout
+                  </Link>
+                </li>
+              ) : (
+                <li>
+                  <Link
+                    className="nav-link nav-link  pt-icon-log-in"
+                    to="/login"
+                  >
+                    Login
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+        {isLoading()}
+      </nav>
+    );
+  }
 }
 
 export default Header;
