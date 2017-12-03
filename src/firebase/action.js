492f57848966eecbc53289a88b3d32c8f08d408b
kevin hu <hu.kevin.w@gmail.com>
2017-12-02 21:08:57 -0500

    Create function to add user

--- src/firebase/action.js
+++ src/firebase/action.js
@@ -1,5 +1,20 @@
 import 'firebase/database';
 import { fire } from '../config/fireConfig';
 
-this.db = fire.database().ref().child('gyms');
-console.log('hello')
+// firebase = fire.database().ref().child('gyms');
+
+export var newUser = (username, useremail) => {
+  fire.database().ref().child('users').push().set({
+    name: username,
+    email: useremail,
+  });
+}
+
+
+
+
+// fire.database().ref('users/' + 1111).set({
+//   username: "kevin",
+//   email: "email",
+//   profile_picture : "imageUrl"
+// });
