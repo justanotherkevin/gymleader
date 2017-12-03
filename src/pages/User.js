f505acfaa44bbbda7fdedbfc441cffbe2335d31b
kevin hu <hu.kevin.w@gmail.com>
2017-12-02 20:39:22 -0500

    Create user page

---
+++ src/pages/User.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Userpage extends Component {
+  render() {
+    return (
+      <div className="userpage-wrapper">
+        <div>
+          <p>user name </p>
+          <p>user other stuff</p>
+        </div>
+
+      </div>
+    )
+  }
+}
+export default Userpage;
